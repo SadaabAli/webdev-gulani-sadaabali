d3d2fc6076686035698534063a60c4aae5b9b0ce
SadaabAli <SadaabAli>
2017-10-27 20:39:19 -0400

    Issue on heroku

--- server.js
+++ server.js
@@ -30,15 +30,13 @@ app.set('port', port);
 // Create HTTP server
 const server = http.createServer(app);
 
-// var serverSide = require("./server/test-mongodb/app");
-// serverSide(app);
+require("./assignment/app.js")(app);
 
 // For Build: Catch all other routes and return the index file -- BUILDING
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'dist/index.html'));
-// });
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './dist/index.html'));
+});
 
-require("./assignment/app.js")(app);
 server.listen( port , () => console.log('Running'));
 
 
