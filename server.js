a8c6ca093a8eeb7610adde220f4b35f1864a5f38
SadaabAli <SadaabAli>
2017-10-27 20:43:39 -0400

    Issue on heroku

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-/**
- * Created by sesha on 6/2/17.
- */
-
-// Get the dependencies
-
 const express = require('express');
 const path = require('path');
 const http = require('http');
@@ -14,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Point static path to dist -- For building -- REMOVE
-// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // CORS
 app.use(function(req, res, next) {
@@ -27,8 +21,12 @@ app.use(function(req, res, next) {
 const port = process.env.PORT || '3100';
 app.set('port', port);
 
+
 // Create HTTP server
 const server = http.createServer(app);
+
+// var serverSide = require("./server/test-mongodb/app");
+// serverSide(app);
 
 require("./assignment/app.js")(app);
 
@@ -37,6 +35,49 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
+
 server.listen( port , () => console.log('Running'));
+
+
+// /**
+//  * Created by sesha on 6/2/17.
+//  */
+//
+// // Get the dependencies
+//
+// const express = require('express');
+// const path = require('path');
+// const http = require('http');
+// const bodyParser = require('body-parser');
+// const app = express();
+//
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+//
+// // Point static path to dist -- For building -- REMOVE
+// // app.use(express.static(path.join(__dirname, 'dist')));
+//
+// // CORS
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
+//
+// const port = process.env.PORT || '3100';
+// app.set('port', port);
+//
+// // Create HTTP server
+// const server = http.createServer(app);
+//
+// require("./assignment/app.js")(app);
+//
+// // For Build: Catch all other routes and return the index file -- BUILDING
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, './dist/index.html'));
+// });
+//
+// server.listen( port , () => console.log('Running'));
 
 
